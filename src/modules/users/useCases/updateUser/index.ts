c36af22ec28b28d18a9bc27e49c8ec9be7de1bdb
Gustavo <gustavo.souza2000@outlook.com>
2024-04-11 21:23:09 -0300

    fix: adjusts updateUser

--- src/modules/users/useCases/updateUser/index.ts
+++ src/modules/users/useCases/updateUser/index.ts
@@ -6,11 +6,11 @@ import { UpdateUserController } from "./UpdateUserController";
 import { UpdateUserUseCase } from "./UpdateUserUseCase";
 
 const updateUserSchema = z.object({
-  name: z.string().min(3, { message: 'O nome deve ter pelo menos 3 letras' }).optional(),
-  email: z.string().email('Formato de e-mail inválido').optional(),
-  currentPassword: z.string().min(6, { message: 'A senha deve ter pelo menos 6 letras' }).optional(),
-  newPassword: z.string().min(6, { message: 'A senha deve ter pelo menos 6 letras' }).optional(),
-  confirmPassword: z.string().min(6, { message: 'A senha deve ter pelo menos 6 letras' }).optional(),
+  name: z.string().min(3, { message: 'O nome deve ter pelo menos 3 letras' }).optional().or(z.literal('')),
+  email: z.string().email('Formato de e-mail inválido').optional().or(z.literal('')),
+  currentPassword: z.string().min(6, { message: 'A senha deve ter pelo menos 6 letras' }).optional().or(z.literal('')),
+  newPassword: z.string().min(6, { message: 'A senha deve ter pelo menos 6 letras' }).optional().or(z.literal('')),
+  confirmPassword: z.string().min(6, { message: 'A senha deve ter pelo menos 6 letras' }).optional().or(z.literal('')),
 
 }).refine(data => data.newPassword === data.confirmPassword, {
   message: 'As senhas precisam ser iguais',
